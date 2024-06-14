18806f3d2d51d204376e43b846459f7117dd338f
Richard Vidal Gutierrez <137844632+RvidalSt4rtend@users.noreply.github.com>
2024-06-14 17:15:10 -0500

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -3,10 +3,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 
 settings_module = "core.settings.deployment" if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings.local'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
-application = WhiteNoise(application)

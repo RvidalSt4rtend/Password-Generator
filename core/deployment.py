76901e2bb0efbc02b712551b86f44dfe97b29838
Richard <rvidal.gutierrez@outlook.com>
2024-06-15 15:58:38 -0500

    update

--- core/deployment.py
+++ core/deployment.py
@@ -110,10 +110,6 @@ STORAGES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

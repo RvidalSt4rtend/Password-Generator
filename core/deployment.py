bb7ffe451f57032848e4d2c95fae7980428f0447
Richard <rvidal.gutierrez@outlook.com>
2024-06-15 15:57:39 -0500

    asda

--- core/deployment.py
+++ core/deployment.py
@@ -107,9 +107,13 @@ STORAGES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
